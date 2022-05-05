6aa1eb8ded2695c18801ba9c1fecb78e7f213995
Alunos <hariel96@gmail.com>
2022-05-05 20:43:42 -0300

    Criação de esquema

--- server.js
+++ server.js
@@ -1,5 +1,7 @@
 var http = require('http');
 var app = require('./config/express')();
-http.createServer(app).listen(app.get('port'), function(){
-	console.log('Express Server escutando na porta ' + app.get('port'));
+const url = 'mongodb://dswa5:dswa5@cluster0-shard-00-00.ig8hl.mongodb.net:27017,cluster0-shard-00-01.ig8hl.mongodb.net:27017,cluster0-shard-00-02.ig8hl.mongodb.net:27017/ifsp?ssl=true&replicaSet=atlas-y7duym-shard-0&authSource=admin&retryWrites=true&w=majority';
+require('./config/database.js')(url);
+http.createServer(app).listen(app.get('port'), function() {
+    console.log('Express Server escutando na porta ' + app.get('port'));
 });
